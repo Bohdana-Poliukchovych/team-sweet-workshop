6c2d3bff4e1f7722cd9010ad0e696e3b4819c957
Olga <olgadubinova8@gmail.com>
2026-05-24 16:24:31 +0300

    Add swiper

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -1,4 +1,52 @@
 import Swiper from 'swiper';
 import 'swiper/css';
+import { Raty } from 'raty-js';
 
 import { getFeedbacks } from './services/api.js';
+const swiperWrapperEl = document.querySelector('swiper-wrapper');
+document.addEventListener('DOMContentLoaded', initReviewsSection);
+async function initReviewsSection() {
+  try {
+    const feedbacks = await getFeedbacks();
+    console.log(1);
+    filteredFeedbacks = feedbacks.slice(0, 10);
+    renderMarkup(filteredFeedbacks);
+    initSwiper();
+  } catch (error) {
+    console.log(error);
+  }
+}
+function renderMarkup(reviews) {
+  const markup = reviews
+    .map(
+      ({ rate, description, author }) => `
+    <div class="swiper-slide review-card">
+    <div data-raty data-score="${rate}"></div>
+    <p class="review-paragraph">${description}</p>
+    <p class="review-author">${author}</p>
+    </div> `
+    )
+    .join('');
+  swiperWrapperEl.innerHTML = markup;
+}
+function initSwiper() {
+  const swiper = new Swiper('.reviews-swiper', {
+    cssMode: true,
+    slidesPerView: 1,
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+      },
+    },
+    navigation: {
+      nextEl: '.swiper-button-prev',
+      prevEl: '.swiper-button-next',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+}
