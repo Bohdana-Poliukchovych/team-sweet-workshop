576836c67254340f8cc3dc4d9deb7c3794714614
Viktor_Shcherbyna <ahmatov@ukr.net>
2026-05-26 10:52:47 +0300

    added DOM elements

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -1,11 +1,15 @@
-// const domSelector = {
-//   openMobileMenu: document.querySelector('.js-open-mobile-menu'),
-//   mobileMenu: document.querySelector('.js-mobile-menu'),
-//   mobileMenuLogo: document.querySelector('.js-logo-mobile-menu'),
-//   closeMobileMenu: document.querySelector('.js-mobile-menu'),
-//   mobileMenu: document.querySelector('.js-mobile-menu'),
-//   mobileMenu: document.querySelector('.js-mobile-menu'),
-//   mobileMenu: document.querySelector('.js-mobile-menu'),
-//   mobileMenu: document.querySelector('.js-mobile-menu'),
-//   mobileMenuBody: document.querySelector('.js-mobile-menu'),
-// };
+const domSelector = {
+  openMobileMenu: document.querySelector('.js-open-mobile-menu'),
+  closeMobileMenu: document.querySelector('.js-mobile-menu'),
+  mobileMenu: document.querySelector('.js-mobile-menu'),
+  logoMobileMenu: document.querySelector('.js-logo-mobile-menu'),
+  linkDessertMobileMenu: document.querySelector('.js-link-mobile-menu-dessert'),
+  linkAboutMobileMenu: document.querySelector('.js-link-mobile-menu-about'),
+  linkFeedbackMobileMenu: document.querySelector(
+    '.js-link-mobile-menu-feedback'
+  ),
+  linkQuestionsMobileMenu: document.querySelector(
+    '.js-link-mobile-menu-questions'
+  ),
+  mobileMenuBody: document.querySelector('body'),
+};
