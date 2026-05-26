2f5f4a9fde3fea945b5520f409e987c035076fc7
Viktor_Shcherbyna <ahmatov@ukr.net>
2026-05-26 16:17:32 +0300

    added the js in the mobile-menu

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -1,15 +1,35 @@
-const domSelector = {
+const domSelectors = {
   openMobileMenu: document.querySelector('.js-open-mobile-menu'),
-  closeMobileMenu: document.querySelector('.js-mobile-menu'),
+  closeMobileMenu: document.querySelector('.js-close-mobile-menu'),
   mobileMenu: document.querySelector('.js-mobile-menu'),
   logoMobileMenu: document.querySelector('.js-logo-mobile-menu'),
-  linkDessertMobileMenu: document.querySelector('.js-link-mobile-menu-dessert'),
-  linkAboutMobileMenu: document.querySelector('.js-link-mobile-menu-about'),
-  linkFeedbackMobileMenu: document.querySelector(
-    '.js-link-mobile-menu-feedback'
-  ),
-  linkQuestionsMobileMenu: document.querySelector(
-    '.js-link-mobile-menu-questions'
-  ),
+  linkMobileMenu: document.querySelectorAll('.js-mobile-menu-link'),
+  btnShoppingMobileMenu: document.querySelector('.js-mobile-menu-shopping-btn'),
   mobileMenuBody: document.querySelector('body'),
 };
+domSelectors.logoMobileMenu.addEventListener('click', closeMenu);
+domSelectors.btnShoppingMobileMenu.addEventListener('click', closeMenu);
+domSelectors.openMobileMenu.addEventListener('click', toggleMenu);
+domSelectors.closeMobileMenu.addEventListener('click', toggleMenu);
+document.addEventListener('keydown', toggleMenu);
+
+function toggleMenu(event) {
+  if (
+    domSelectors.mobileMenu.classList.contains('is-open') &&
+    event.key === 'Escape'
+  ) {
+    domSelectors.mobileMenu.classList.remove('is-open');
+    domSelectors.mobileMenuBody.classList.remove('no-scroll');
+    return;
+  }
+  domSelectors.mobileMenu.classList.toggle('is-open');
+  domSelectors.mobileMenuBody.classList.toggle('no-scroll');
+}
+
+domSelectors.linkMobileMenu.forEach(link => {
+  link.addEventListener('click', closeMenu);
+});
+function closeMenu() {
+  domSelectors.mobileMenu.classList.remove('is-open');
+  domSelectors.mobileMenuBody.classList.remove('no-scroll');
+}
