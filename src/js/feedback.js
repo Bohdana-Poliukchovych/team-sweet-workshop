754da839948bfdadeced91edd60f5a396df021ae
Olga <olgadubinova8@gmail.com>
2026-05-27 17:55:54 +0300

    fix render

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -12,8 +12,8 @@ const swiperWrapperEl = document.querySelector(
 initReviewsSection();
 async function initReviewsSection() {
   try {
-    const { feedbacks } = await getFeedbacks();
-    renderMarkup(feedbacks.slice(0, 10));
+    const { feedbacks } = await getFeedbacks(10, 1);
+    renderMarkup(feedbacks);
     initSwiper();
   } catch (error) {
     console.log(error);
